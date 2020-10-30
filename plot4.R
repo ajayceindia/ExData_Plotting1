47e025631650e0b47e6cafe0894c5bff4b061e58
ajayceindia <59169650+ajayceindia@users.noreply.github.com>
2020-10-31 00:56:51 +0530

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,52 @@
+if(!dir.exists("data")) { dir.create("data") }
+
+file.url   <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+file.path  <- "data/household_power_consumption.zip"
+file.unzip <- "data/household_power_consumption.txt"
+
+if(!file.exists(file.path) & !file.exists(file.unzip)) {
+  download.file(file.url, file.path)
+  unzip(file.path, exdir = "data")
+}
+
+
+t <- read.table("data/household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+t$Date <- as.Date(t$Date, "%d/%m/%Y")
+
+t <- subset(t,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+t <- t[complete.cases(t),]
+
+dateTime <- paste(t$Date, t$Time)
+
+dateTime <- setNames(dateTime, "DateTime")
+
+t <- t[ ,!(names(t) %in% c("Date","Time"))]
+
+t <- cbind(dateTime, t)
+
+t$dateTime <- as.POSIXct(dateTime)
+
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(t, {
+  plot(Global_active_power~dateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~dateTime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~dateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~dateTime,col='Red')
+  lines(Sub_metering_3~dateTime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~dateTime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+dev.copy(png, "plot4.png",
+         width  = 480,
+         height = 480)
+
+dev.off()
+
+rm(list = ls())
